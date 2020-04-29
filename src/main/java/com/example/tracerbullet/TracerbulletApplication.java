9d105c453738779c92ba873d98790253ad6b77b1
Andre Browne <abrowne@pivotal.io>
2020-04-29 11:52:52 -0400

    Initial commit

---
+++ src/main/java/com/example/tracerbullet/TracerbulletApplication.java
@@ -0,0 +1,13 @@
+package com.example.tracerbullet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TracerbulletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TracerbulletApplication.class, args);
+	}
+
+}
